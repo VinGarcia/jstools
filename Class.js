2cf2632f438f501ecce2e6106f200341037f6b78
Vinícius Garcia <vingarcia00@gmail.com>
2015-08-10 11:46:02 -0300

    On extending copy objects to instance and not to prototype (except functions) Prepare Class to allow multiple inheritance by using apply() Add (new Class).apply(<Class constructor>) syntatic sugar

--- Class.js
+++ Class.js
@@ -7,47 +7,86 @@
   var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\bsuper\b/ : /.*/;
  
   // The base Class implementation (does nothing)
-  this.Class = function(){};
- 
+  this.Class = function(){ this.apply = apply };
+
+  function apply(func, args) {
+    func.apply(this, [].splice.call(arguments, 1));
+    return this;
+  }
+
   // Create a new Class that inherits from this class
   Class.extend = function(prop) {
     var _super = this.prototype;
+    var constructor = this
    
     // Instantiate a base class (but only create the instance,
     // don't run the init constructor)
     initializing = true;
     var prototype = new this();
     initializing = false;
-   
-    // Copy the properties over onto the new prototype
+
+    // Copy the functions to the prototype:
     for (var name in prop) {
-      // Check if we're overwriting an existing function
-      prototype[name] = typeof prop[name] == "function" &&
-        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-        (function(name, fn){
-          return function() {
-            var tmp = this.super;
-           
-            // Add a new .super() method that is the same method
-            // but on the super-class
-            this.super = _super[name];
-           
-            // The method only need to be bound temporarily, so we
-            // remove it when we're done executing
-            var ret = fn.apply(this, arguments);
-            this.super = tmp;
-           
-            return ret;
-          };
-        })(name, prop[name]) :
-        prop[name];
+      if( typeof prop[name] == 'function' ) {
+        // Check if we're overwriting an existing function
+        prototype[name] = typeof _super[name] == "function" &&
+          fnTest.test(prop[name]) ?
+          (function(name, fn){
+            return function() {
+              var tmp = this.super;
+             
+              // Add a new .super() method that is the same method
+              // but on the super-class
+              this.super = _super[name]
+             
+              // The method only need to be bound temporarily, so we
+              // remove it when we're done executing
+              var ret = fn.apply(this, arguments);
+              this.super = tmp;
+             
+              return ret;
+            };
+          })(name, prop[name]) : prop[name]
+      }
     }
    
     // The dummy class constructor
     function Class() {
       // All construction is actually done in the init method
-      if ( !initializing && this.init )
-        this.init.apply(this, arguments);
+      if ( !initializing ) {
+        // Call the parent constructors:
+        constructor.apply(this)
+
+        // Copy the properties over onto the new prototype:
+        for (var name in prop) {
+          if( typeof prop[name] != 'function')
+            // If name is not defined:
+            if(!this[name])
+              this[name] = typeof prop[name] == 'object' ?
+                prop[name].copy() : prop[name]
+        }
+
+        // Try to insert the prototype into this object
+        // if its being called with apply()
+        if(!this.instanceof(arguments.callee)) {
+          var proto = arguments.callee.prototype.copy()
+          var this_proto = Object.getPrototypeOf(this)
+          Object.setPrototypeOf(proto, this_proto)
+          Object.setPrototypeOf(this, proto)
+        }
+
+        if( prop.init )
+          prop.init.apply(this, arguments);
+
+        // Add the apply utility to this:
+        Object.defineProperties(this, {
+          apply : {
+            value : apply,
+            writable: false,
+            enumerable : false
+          }
+        })
+      }
     }
    
     // Populate our constructed prototype object
