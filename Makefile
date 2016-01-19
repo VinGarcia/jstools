7d2e6d8bd9da552798bd429a8640e2d455d46c73
Vinícius Garcia <vingarcia00@gmail.com>
2016-01-19 13:45:53 -0200

    Add test suit with mocha for `copy.js`

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+
+
+test:
+	mocha copy.test.js
+
+install:
+	sudo npm install -g mocha
+	sudo npm install should --save-dev
---
+++ copy.test.js
@@ -0,0 +1,25 @@
+
+require('should')
+
+copy = require('./copy.js').copy
+
+var assert = require('assert');
+describe('#copy', function() {
+  describe('#literal', function() {
+    it('should return the exact same object', function () {
+      copy(0).should.be.exactly(0)
+      copy(1).should.be.exactly(1)
+      copy(2).should.be.exactly(2)
+      copy("").should.be.exactly("")
+      copy("123").should.be.exactly("123")
+    });
+  });
+
+  describe('#lists', function() {
+    it('should not be undefined', function () {
+      a = {}
+      var should = require('should').noConflict()
+      should(a.copy).not.be.ok()
+    });
+  });
+});
